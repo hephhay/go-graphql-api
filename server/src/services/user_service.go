eb7aeb932d07e976bcd8922b58d398c2f4f51c55
hephhay <faradaydanfard@gmail.com>
2023-06-03 10:40:43 -0100

    update: base class, concrete class

---
+++ server/src/services/user_service.go
@@ -0,0 +1,22 @@
+package services
+
+import (
+	"github.com/hephhay/go-graphql/src/abstracts"
+	"github.com/hephhay/go-graphql/src/repositories"
+)
+
+type UserServiceInterface interface {
+	abstracts.BaseServiceInterface
+}
+
+type UserService struct {
+	*abstracts.BaseServiceInterface
+	repository repositoies.UserRepositoryInterface
+}
+
+// @Summary UserService constructor
+func NewUserService(repository repositories.UserRepositoryInterface) UserServiceInterface {
+	baseService := abstracts.NewBaseService(repository).(*abstracts.BaseService)
+	service := &UserService{baseService, repository}
+	return service
+}
