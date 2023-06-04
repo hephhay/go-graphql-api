05f6c755657511ca4db6c1fb3c12db009d7047c3
hephhay <faradaydanfard@gmail.com>
2023-06-04 00:47:46 -0100

    update: user controller

--- server/app/controllers/user_controller.go
+++ server/app/controllers/user_controller.go
@@ -1,1 +1,18 @@
 package controllers
+
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/hephhay/go-graphql/src/abstracts"
+	"github.com/hephhay/go-graphql/src/services"
+)
+
+type UserController struct {
+	*abstracts.BaseController
+	service service.UserServiceInterface
+}
+
+// @Summary UserController constructor
+func NewUserController(service service.UserServiceInterface) *UserController {
+	controller := abstracts.NewBaseController(service)
+	return &UserController{BaseController: controller, service: service}
+}
