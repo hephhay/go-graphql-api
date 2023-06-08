924bf81a6b40c39513981485169a57072cba84e2
hephhay <faradaydanfard@gmail.com>
2023-06-08 17:44:10 -0100

    fix: usr controller, service and repository

--- server/app/controllers/user_controller.go
+++ server/app/controllers/user_controller.go
@@ -1,18 +1,17 @@
 package controllers
 
 import (
-	"github.com/gin-gonic/gin"
 	"github.com/hephhay/go-graphql/src/abstracts"
 	"github.com/hephhay/go-graphql/src/services"
 )
 
 type UserController struct {
 	*abstracts.BaseController
-	service service.UserServiceInterface
+	service services.UserServiceInterface
 }
 
 // @Summary UserController constructor
-func NewUserController(service service.UserServiceInterface) *UserController {
+func NewUserController(service services.UserServiceInterface) *UserController {
 	controller := abstracts.NewBaseController(service)
 	return &UserController{BaseController: controller, service: service}
 }
