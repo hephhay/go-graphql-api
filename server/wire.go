9319e4b39fd92dbddc03d24956aa8fe452b97ae3
hephhay <faradaydanfard@gmail.com>
2023-06-09 21:58:16 -0100

    chore: bind depedencies with google wire

--- server/wire.go
+++ server/wire.go
@@ -1,13 +1,26 @@
-// +build wireinject
+//+build wireinject
 
 package main
 
 import (
 	"github.com/google/wire"
+	"github.com/hephhay/go-graphql/app/controllers"
+	"github.com/hephhay/go-graphql/configs"
+	"github.com/hephhay/go-graphql/database"
+	"github.com/hephhay/go-graphql/src/repositories"
+	"github.com/hephhay/go-graphql/src/services"
+	"github.com/hephhay/go-graphql/app"
 )
 
-func InitializeApp(dbCfg string) (Application, error) {
-	panic(wire.Build(ProvideControllers, ProvideServices, ProvideRepositories, ProvideDBConnection))
+func InitializeApp(dbCfg *configs.DBCfg) (*Application, error){
+	wire.Build(
+		database.NewDBConnection,
+		repositories.NewUserRepository,
+		services.NewUserService,
+		controllers.NewUserController,
+		app.NewRouter,
+		NewApplication,
+	)
 
-	return Application{}, nil
+	return nil, nil
 }
