4bd9326a0f3d100f4b9827d75b66d69742f46d71
hephhay <faradaydanfard@gmail.com>
2023-06-15 05:01:43 -0100

    fix: dependencies injection

--- server/cmd/server/wire.go
+++ server/cmd/server/wire.go
@@ -4,16 +4,19 @@ package server
 
 import (
 	"github.com/google/wire"
+	"github.com/hephhay/go-graphql/config"
 	"github.com/hephhay/go-graphql/internal/app"
 	"github.com/hephhay/go-graphql/internal/repository"
 	"github.com/hephhay/go-graphql/internal/service"
+	"github.com/hephhay/go-graphql/internal/resolver"
 )
 
-func InitializeApp(appCfg *config.AppCfg, dbCfg *config.DBCfg) (*Application, error) {
+func InitializeApp(dbCfg *config.DBCfg) (*Application, error) {
 	wire.Build(
 		app.NewDBConnection,
 		repository.NewUserRepository,
 		service.NewUserService,
+		resolver.NewUserResolver,
 		app.NewRoute,
 		NewApplication,
 	)
--- server/internal/app/routes.go
+++ server/internal/app/routes.go
@@ -1,28 +1,28 @@
 package app
 
 import (
+	"github.com/99designs/gqlgen/graphql/handler"
+	"github.com/99designs/gqlgen/graphql/playground"
 	"github.com/gin-gonic/gin"
-	"github.com/hephhay/go-graphql/internal/resolver"
-	"github.com/hephhay/go-graphql/internal/service"
-	"github.com/99designs/gqlgen/handler"
-	"net/http"
 	"github.com/hephhay/go-graphql/graphql/generated"
+	"github.com/hephhay/go-graphql/internal/resolver"
+	"net/http"
 )
 
-func NewRoute(service service.UserService) *gin.Engine {
+func NewRoute(resolver *resolver.UserResolver) *gin.Engine {
 	r := gin.Default()
 	r.Use(GinContextToContextMiddleware())
 	r.GET("/ping", func(c *gin.Context) {
 		c.String(http.StatusOK, "pong")
 	})
-	r.POST("/query", graphqlHandler(service))
+	r.POST("/query", graphqlHandler(resolver))
 	r.GET("/", playgroundHandler())
 
 	return r
 }
 
-func graphqlHandler(service service.UserService) gin.HandlerFunc {
-	h := handler.NewDefaultServer(generated.NewExecutableSchema(generated.Config{Resolvers: &resolver.NewUserResolver{service}}))
+func graphqlHandler(resolver *resolver.UserResolver) gin.HandlerFunc {
+	h := handler.NewDefaultServer(generated.NewExecutableSchema(generated.Config{Resolvers: resolver}))
 
 	return func(c *gin.Context) {
 		h.ServeHTTP(c.Writer, c.Request)
